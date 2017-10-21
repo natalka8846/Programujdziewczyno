7b687d440ff0421de092eff3a5fc110d65b5c0e3
janiuk.natalia@gmail.com <natalia@student.edu>
2017-10-21 15:58:53 +0200

    teoria js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var myName='Natalia';
+var friends=['Basia', 'Kasia', 'Asia'];
+var a=25;
+var b=20;
+if (a < b) {
+console.log('hurra')
+}
+else {
+console.log('jupi')
+}
+for (var i = 0; i < friends.length; i++) {
+  console.log(friends[i])
+}
